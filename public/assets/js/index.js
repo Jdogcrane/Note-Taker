f8c6f98596d38dc3369a36ffc465fc757b0c7baf
Josh <jdogcrane@gmail.com>
2021-06-15 14:46:41 -0400

    heroku setup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,18 +1,3 @@
-const express = require('express');
-const path = require('path');
-
-// Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -196,4 +181,3 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT} http://localhost:${PORT}`));
---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require("fs");
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const db = require('./db/db.json');
+console.log(db);
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(data);
+});
+
+app.post('/api/notes', (req, res) => {
+    const body = { ...req.body };
+    const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    body.id = uuidv4()
+    fs.writeFileSync('./db/db.json', JSON.stringify(data.concat(req.body), null, 2), 'utf-8');
+    res.send(req.body);
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    const items = data.filter((item) => item.id !== id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(items), 'utf-8');
+    res.send("success");
+})
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT} http://localhost:${PORT}`));
